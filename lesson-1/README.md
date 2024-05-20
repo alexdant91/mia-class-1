fef3d8565171f0001e903b2ebac69171c8bd685f
Alessandro D'Antoni <alexdant91@gmail.com>
2024-05-20 13:07:42 +0200

    Data Types and variables - foundamentals

---
+++ lesson-1/README.md
@@ -0,0 +1,14 @@
+# Data Type & Foundamentals
+
+- Variables;
+    1. let;
+    2. const;
+- `String`;
+- `Number`;
+- `Boolean`;
+- `Array`;
+- `Object`;
+- `Function`;
+- `Date`;
+- `null`;
+- `undefined`;
---
+++ lesson-1/index.js
@@ -0,0 +1,132 @@
+// Variables
+// let counter = 1;
+
+// counter = counter + 1;
+
+// console.log(counter);
+
+// String
+// "abcABC123456/()é+°#"
+// 'abcABC123456/()é+°#'
+// `abcABC123456/()é+°#`
+
+// Number
+// 123
+
+// Boolean
+// true // 1
+// false // 0
+
+// Array
+// const colors = ["red", "blue", "green", "yellow"]; // index = 0 -> +infinito
+
+// console.log(colors);
+
+// colors[1] = "orange";
+// colors[2] = "teal";
+// colors[3] = "gray";
+// colors[4] = "purple";
+
+// console.log(colors);
+
+// console.log(colors.length);
+
+// Object
+// const first_name = "Ale";
+// const key = "name";
+// const keyToDelete = "password";
+
+// const user = {
+//     id: 0,
+//     name: first_name,
+//     age: 32,
+// }
+
+// // Server
+
+// delete user["password"];
+// delete user.password;
+// delete user[keyToDelete];
+
+// console.log(user);
+
+// user[key] = "Giovanni";
+
+// console.log(key, user[key]);
+
+// console.log(user["id"]);
+
+// user.age = 30;
+
+// console.log(user.age);
+
+// Function
+// function sum(a, b) { // |-> DICHIARANDO
+//     return a + b;
+// }
+
+// const sum = (a, b) => { // Dichiaro
+//     console.log(a + b)
+// };
+
+// const a = 1;
+// const b = 2;
+
+// const result = sum(a, b); // |-> ESEGUIRE
+
+// console.log(result);
+
+// Date
+// console.log(new Date());
+// console.log(new Date().toLocaleString());
+// console.log(new Date().getDate());
+// console.log(new Date().getMonth());
+// console.log(new Date().getFullYear());
+// console.log(new Date().getDay());
+// console.log(new Date().getHours());
+// console.log(new Date().getMinutes());
+// console.log(new Date().getSeconds());
+// console.log(new Date().getMilliseconds());
+// console.log(new Date().getTime());
+
+// console.log(new Date(1716202477084).toLocaleString()); // -> 1-1-1970, 00:00:00 -> ora in ms
+
+// null
+// const name = null;
+// const name = "Ale", age = 32, id = 0;
+
+// const user = {
+//     key: id,
+//     first_name: name,
+//     age1: age,
+// }
+
+// const user1 = {
+//     id,
+//     name,
+//     age,
+// }
+
+// console.log(user);
+// console.log(user1);
+
+// undefined
+// const name1 = undefined;
+
+// const data = {
+//     id: 0,
+//     name: "Ale",
+//     age: undefined,
+//     age1: null,
+// }
+
+// data.age = 32; // Init
+// data.age1 = 32; // Update
+
+// console.log(data);
+
+
+
+
+
+
