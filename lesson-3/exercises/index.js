ac8d515f25653a2a649d8ea365eb3391a6e6db2e
SantosIvanAlvaChavez <ivanals8998@hotmail.com>
2024-05-25 15:39:08 +0200

    javascript exercises

--- lesson-3/exercises/index.js
+++ lesson-3/exercises/index.js
@@ -6,28 +6,45 @@
  * es: "bolla" -> "allob"; // dispari
  */
 
+const checkStr = (str) => {
+    if(typeof str !== "string") throw new Error(`the ${str} parameter must be a string`);
+
+    return (str.length % 2 === 0) ? str : str.split("").reverse().join("");
+}
+
+console.log(checkStr("ciao"));
+console.log(checkStr("bolla"));
+
 /**
  * 2. un arrow function che accetta un array di oggetti utenti (innestati) e restituisce un array di email
- * 
- * const users = [
- *      [
- *          { id: 0, name: "Giovanni", age: 15, userLevel: 1, email: "giovanni.rossi@gmail.com" },
- *          { id: 1, name: "Giorgio", age: 25, userLevel: 2, email: "giorgio.bianchi@gmail.com" },
- *          { id: 2, name: "Elena", age: 18, userLevel: 0, email: "elena.verdi@gmail.com" },
- *          { id: 3, name: "Manfredi", age: 19, userLevel: 4, email: "manfredi.rossi@gmail.com" },
- *      ],
- *      [
- *          { id: 4, name: "Arturo", age: 13, userLevel: 2, email: "arturo.bianchi@gmail.com" },
- *      ],
- *      [
- *          { id: 5, name: "Andrea", age: 23, userLevel: 1, email: "andrea.verdi@gmail.com" },
- *          [
- *              { id: 6, name: "Elisa", age: 17, userLevel: 3, email: "elisa.rossi@gmail.com" },
- *          ]
- *      ]
- * ];
  */
 
+const users = [
+    [
+        { id: 0, name: "Giovanni", age: 15, userLevel: 1, email: "giovanni.rossi@gmail.com" },
+        { id: 1, name: "Giorgio", age: 25, userLevel: 2, email: "giorgio.bianchi@gmail.com" },
+        { id: 2, name: "Elena", age: 18, userLevel: 0, email: "elena.verdi@gmail.com" },
+        { id: 3, name: "Manfredi", age: 19, userLevel: 4, email: "manfredi.rossi@gmail.com" },
+    ],
+    [
+        { id: 4, name: "Arturo", age: 13, userLevel: 2, email: "arturo.bianchi@gmail.com" },
+    ],
+    [
+        { id: 5, name: "Andrea", age: 23, userLevel: 1, email: "andrea.verdi@gmail.com" },
+        [
+            { id: 6, name: "Elisa", age: 17, userLevel: 3, email: "elisa.rossi@gmail.com" },
+        ]
+    ]
+];
+
+const getEmailArray = (array) => {
+    if( !Array.isArray(array) ) throw new Error(`the ${array} parameter must be an array`);
+
+    return array.flat(Infinity).map((item) => item.email);
+}
+
+console.log(getEmailArray(users));
+ 
 /**
  * 3. un arrow function che accetta un paramentro array e un parametro stringa che può essere solo "ASC" e "DESC" e restituisce 
  * l'array ordinato in ordine alfabeto crescente o descrescente in base a valore del secondo parametro:
@@ -42,3 +59,14 @@
  * const colors = ["red", "blue", "green", "yellow", "orange", "purple", "black", "white"];
  * 
  */
+
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "black", "white"];
+
+const getSortArray = (array, mode = "ASC") => {
+    if (mode.toUpperCase() === "ASC") return array.sort();
+    else if (mode.toUpperCase() === "DESC") return array.sort().reverse();
+    else throw new Error(`the ${mode} parameter must be "asc" or "desc"`);
+}
+  
+console.log(getSortArray(colors, "ASC")); 
+console.log(getSortArray(colors, "DESC"));
