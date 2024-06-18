377ca320d152db7295ab6936f114f835117a4fb1
Alessandro D'Antoni <alexdant91@gmail.com>
2024-06-18 10:58:26 +0200

    fix: if statement

--- lesson-12/index.js
+++ lesson-12/index.js
@@ -3,20 +3,18 @@ const $accordion = document.querySelector("#accordion-1");
 const init = () => {
     $accordion.querySelectorAll(".accordion__item-title").forEach($el => {
         $el.addEventListener("click", (e) => {
-            if (e.target.classList.contains("accordion__item-title")) {
-                const $this = e.target;
-                const $openedItemBody = $accordion.querySelector(".accordion__item.open");
-                const $currentItem = $this.parentElement;
-    
-                if ($currentItem.classList.contains("open")) {
-                    $currentItem.classList.remove("open");
-                    return;    
-                }
-    
-                if ($openedItemBody) $openedItemBody.classList.remove("open");
-                
-                $currentItem.classList.add("open");
+            const $this = e.target;
+            const $openedItemBody = $accordion.querySelector(".accordion__item.open");
+            const $currentItem = $this.parentElement;
+
+            if ($currentItem.classList.contains("open")) {
+                $currentItem.classList.remove("open");
+                return;    
             }
+
+            if ($openedItemBody) $openedItemBody.classList.remove("open");
+            
+            $currentItem.classList.add("open");
         });
     });
 }
