282443d53ddf9ad44ee0840ed9d969ac63c4fb2c
Zaccaria <zac.fede96@gmail.com>
2024-06-24 13:05:00 +0200

    Es live counter correzione

--- lesson-13/federico.js
+++ lesson-13/federico.js
@@ -32,8 +32,8 @@
 
 
 const counterFunction = (num) => {
-  if (!num || typeof num !== "number" ) {
-    throw new Error(`${typeof num} is not valid`);
+  if (typeof num === "number" && num > 9999) {
+    throw new Error(`this number is not valid, il numero dev'essere intero e inferiore a 9999`);
   }return num.toString().length;
 }
 console.log(counterFunction(125));
